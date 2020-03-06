13ed87a2fcf3fe5e2e7c77c65eda8e9ad4b28541
Samuel Thompson <thomps9012@gmail.com>
2020-03-06 08:36:55 -0500

    revamping and updating changes

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,42 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var characternum = prompt ("How many characters long would you like your password?");
+
+if (confirm("Lowercase")) {
+  txt = " ";
+} else {
+  txt = " ";
+}
+
+if (confirm("Uppercase")) {
+  txt = " ";
+} else {
+  txt = " ";
+}
+
+if (confirm("Numeric")) {
+  txt = " ";
+} else {
+  txt = " ";
+}
+
+if (confirm("Special Characters")) {
+  txt = " ";
+} else {
+  txt = " ";
+}
+
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
+
+   
+  
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  var character = prompt("How many characters in your password?")
 
 }
 
